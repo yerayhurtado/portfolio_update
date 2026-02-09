4c60e4a84f9178e641280c02f996888e47c09dfd
yerayhurtado <yerayhudra13@gmail.com>
2026-02-09 17:46:32 +0100

    Update Contact.tsx

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -33,7 +33,7 @@ export default function Contact() {
       className="py-32 px-6 bg-[#121212] relative overflow-hidden"
     >
       {/* Luces de fondo optimizadas para diseño centrado */}
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[600px] h-[600px] bg-[#00D4FF]/5 rounded-full blur-[120px] pointer-events-none" />
+      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-150 h-150 bg-[#00D4FF]/5 rounded-full blur-[120px] pointer-events-none" />
 
       <div className="max-w-4xl mx-auto relative z-10 text-center">
         {/* Cabecera */}
